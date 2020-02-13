bd55e37cea0576dd1fbae736543d2178e582cbe9
gfellah45 <jerry.aaron45@gmail.com>
2020-02-13 22:25:15 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.querySelector('.bars-wrapper').addEventListener('click', () => {
+   document.querySelector('.navigation-menu').classList.toggle('change');
+   document.querySelector('.bars-wrapper').classList.toggle('change');
+    
+});
+
+
+
+
+
+
+
+
+
+
